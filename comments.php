6d9fe6de7a6bce3e08bd70a19ca243b4f143248d
sabirul-islam <sabirul.shimul@gmail.com>
2020-11-25 23:39:07 +0600

    ok

---
+++ comments.php
@@ -0,0 +1,10 @@
+<?php
+// single line comments
+# single line comments
+/*This is multiple
+line comment*/
+echo "first php code</br>";
+echo "Thank you";
+
+# if you use php file you don't need any closing tag
+?>
