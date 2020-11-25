9ce5485f24efea927af43c2ad1542e7b6e56295e
sabirul-islam <sabirul.shimul@gmail.com>
2020-11-26 03:30:27 +0600

    ok

---
+++ Thapa Technical/arithmeticOperator.php
@@ -0,0 +1,22 @@
+<?php
+#arithmetic operator => +-*/%
+$num1 = 100;
+$num2 = 40;
+
+$sum1 = $num1 + $num2;
+$sum2 = $num1 - $num2;
+$sum3 = $num1 * $num2;
+$sum4 = $num1 / $num2;
+$sum5 = $num1 % $num2;
+
+echo "Total : $sum1";
+echo "<br/>";
+echo "Difference : $sum2";
+echo "<br/>";
+echo "Multiplication : $sum3";
+echo "<br/>";
+echo "Division : $sum4";
+echo "<br/>";
+echo "Modolus : $sum5";
+
+?>
---
+++ Thapa Technical/power.php
@@ -0,0 +1,7 @@
+<?php
+$base = 5;
+$power = 3; #u can also negetive value also
+$powerOfNumber = pow($base, $power);
+echo "$powerOfNumber"
+
+?>
